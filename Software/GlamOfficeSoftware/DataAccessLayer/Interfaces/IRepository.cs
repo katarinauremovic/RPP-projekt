7d93dae30250a77742a6ec3cdaef3594140e4673
mbanovic20 <mbanovic21@foi.hr>
2025-01-13 12:36:16 +0100

    IRepository<T> i Repository<T> napravljeni

---
+++ Software/GlamOfficeSoftware/DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task AddAsync(T item);
+
+        Task RemoveAsync(T item);
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ Software/GlamOfficeSoftware/DataAccessLayer/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public abstract class Repository<T> : IRepository<T>, IDisposable where T : class
+    {
+        private GlamOfficeDbContext _context { get; set; }
+        protected DbSet<T> items { get; set; }
+
+        protected Repository()
+        {
+            _context = new GlamOfficeDbContext();
+            items = _context.Set<T>();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await items.ToListAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await items.FindAsync(id);
+        }
+
+        public virtual async Task AddAsync(T item)
+        {
+            items.Add(item);
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task RemoveAsync(T item)
+        {
+            items.Remove(item);
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
