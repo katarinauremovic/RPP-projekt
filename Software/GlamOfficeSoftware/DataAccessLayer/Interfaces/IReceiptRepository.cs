f1eeeb58935891248a6e61a520e590bf254a8441
mbanovic20 <mbanovic21@foi.hr>
2025-01-17 18:08:56 +0100

    počinjem ispočetka.

--- Software/GlamOfficeSoftware/DataAccessLayer/Interfaces/IReceiptRepository.cs
+++ Software/GlamOfficeSoftware/DataAccessLayer/Interfaces/IReceiptRepository.cs
@@ -9,16 +9,6 @@ namespace DataAccessLayer.Interfaces
 {
     public interface IReceiptRepository
     {
-        string GenerateReceiptNumber();
-        Task<bool> IsClientsGiftCard(int giftCardId, string promoCode);
-        Task<IEnumerable<Receipt>> GetReceiptsByClientAsync(int clientId);
-        Task<bool> ExistsAsync(int receiptId);
-        Task<Receipt> GenerateReceiptAsync(int reservationId);
-        Task<IEnumerable<Receipt>> GetReceiptsByDateRangeAsync(DateTime startDate, DateTime endDate);
-        Task ApplyDiscountAsync(int receiptId, decimal discountAmount);
-        Task<Receipt> GetReceiptByIdAsync(int receiptId);
-        Task<IEnumerable<Receipt>> GetReceiptsByTreatmentAsync(int treatmentId);
-        Task<decimal> GetTotalRevenueAsync();
-        Task<IEnumerable<Receipt>> GetReceiptsByEmployeeAsync(int employeeId);
+        
     }
 }
