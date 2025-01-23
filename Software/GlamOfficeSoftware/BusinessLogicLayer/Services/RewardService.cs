1a55fc3d696aef74b1dce6c1c5f120092ec8f141
mbanovic20 <mbanovic21@foi.hr>
2025-01-23 22:42:42 +0100

    priprema za dalje

---
+++ Software/GlamOfficeSoftware/BusinessLogicLayer/Services/RewardService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogicLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class RewardService : IRewardService
+    {
+
+    }
+}
