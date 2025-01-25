0926f61859380b08c6a594808c564a9a176d25f1
mbanovic20 <mbanovic21@foi.hr>
2025-01-25 16:52:22 +0100

    napravljene boje ovisno o loyalty levelu

---
+++ Software/GlamOfficeSoftware/PresentationLayer/Converters/LoyaltyLevelToColorConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PresentationLayer.Converters
+{
+    public class LoyaltyLevelToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string level = value as string;
+            if (level == null)
+                return Brushes.Transparent;
+
+            switch (level.ToLower())
+            {
+                case "gold":
+                    return Brushes.Gold;
+                case "silver":
+                    return Brushes.Silver;
+                case "bronze":
+                    return Brushes.SandyBrown;
+                case "platinum":
+                    return Brushes.Plum;
+                case "vip":
+                    return Brushes.MediumPurple;
+                default:
+                    return Brushes.Transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
