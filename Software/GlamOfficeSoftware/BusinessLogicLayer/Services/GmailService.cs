48c4d6d9790beeb426f6bc50605a5200875724ca
mbanovic20 <mbanovic21@foi.hr>
2025-01-30 20:09:30 +0100

    Refaktoriran gmailService

--- Software/GlamOfficeSoftware/BusinessLogicLayer/Services/GmailService.cs
+++ Software/GlamOfficeSoftware/BusinessLogicLayer/Services/GmailService.cs
@@ -11,14 +11,14 @@ using MailKit.Net.Smtp;
 
 namespace BusinessLogicLayer.Services
 {
-    public class GmailService
+    public abstract class GmailService
     {
         protected readonly string _email = "glamoffice2025@gmail.com";
         protected readonly string _password = "zztv yfus xomu btrp";
         protected readonly string _smtpServer = "smtp.gmail.com";
         protected readonly int _smtpPort = 587;
 
-        public async Task SendEmailAsync(string recipientEmail, string subject, string body)
+        public virtual async Task SendEmailAsync(string recipientEmail, string subject, string body)
         {
             var email = new MimeMessage();
             email.From.Add(new MailboxAddress("Glam Office", _email));
--- Software/GlamOfficeSoftware/BusinessLogicLayer/Services/RewardSystem.cs
+++ Software/GlamOfficeSoftware/BusinessLogicLayer/Services/RewardSystem.cs
@@ -91,37 +91,8 @@ namespace BusinessLogicLayer.Services
         { 
             var rewards = await GetRewardsDtoForClientAsync(client.idClient);
 
-            string subject = $"Congratulations! Your new Loyalty Level is {loyaltyLevel.Name}";
-
-            string body = $"Hello {client.Firstname} {client.Lastname},<br><br>" +
-                          $"Your loyalty level has been upgraded to: <b>{loyaltyLevel.Name}</b>!<br>" +
-                          $"With this level, you are eligible for the following rewards:<br><br>" +
-                          "<table border='1' cellpadding='10' style='border-collapse: collapse;'>" +
-                          "<thead><tr>" +
-                          "<th>Reward</th>" +
-                          "<th>Description</th>" +
-                          "<th>Cost (Points)</th>" +
-                          "<th>Reedem Code</th>" +
-                          "<th>Status</th>" +
-                          "</tr></thead>" +
-                          "<tbody>";
-
-            foreach (var reward in rewards)
-            {
-                body += $"<tr>" +
-                        $"<td>{reward.Name}</td>" +
-                        $"<td>{reward.Description}</td>" +
-                        $"<td>{reward.CostPoints}</td>" +
-                        $"<td>{reward.ReedemCode}</td>" +
-                        $"<td>{reward.Status}</td>" +
-                        "</tr>";
-            }
-
-            body += "</tbody></table><br><br>" +
-                    "Thank you for being a loyal customer!";
-
-            var gmailService = new GmailService();
-            gmailService.SendEmailAsync(client.Email, subject, body).Wait();
+            var clientGmailService = new ClientGmailService();
+            await clientGmailService.FormatLoyaltyLevelUpgradeEmail(client, loyaltyLevel, rewards);
 
             Console.WriteLine("Loyalty level change email successfully sent.");
         }
@@ -139,40 +110,8 @@ namespace BusinessLogicLayer.Services
 
             lostRewards = lostRewards.Distinct().ToList();
 
-            string subject = $"Notice: Your Loyalty Level has been downgraded to {loyaltyLevel.Name}";
-            string body = $"Hello {client.Firstname} {client.Lastname},<br><br>" +
-                          $"Your loyalty level has been changed to: <b>{loyaltyLevel.Name}</b>.<br>" +
-                          $"We value your continued support and encourage you to keep earning points to unlock more rewards.<br><br>";
-
-            if (lostRewards.Any())
-            {
-                body += "Unfortunately, with this change, you no longer have access to the following rewards:<br><br>" +
-                        "<table border='1' cellpadding='10' style='border-collapse: collapse;'>" +
-                        "<thead><tr>" +
-                        "<th>Reward</th>" +
-                        "<th>Description</th>" +
-                        "<th>Cost (Points)</th>" +
-                        "</tr></thead>" +
-                        "<tbody>";
-
-                foreach (var reward in lostRewards)
-                {
-                    body += $"<tr>" +
-                            $"<td>{reward.Name}</td>" +
-                            $"<td>{reward.Description}</td>" +
-                            $"<td>{reward.CostPoints}</td>" +
-                            "</tr>";
-                }
-
-                body += "</tbody></table><br>";
-            }
-
-            body += "<br>Thank you for your loyalty!<br>" +
-                    "Keep earning points to reach higher levels again.<br><br>" +
-                    "Best regards,<br>Your Loyalty Program Team";
-
-            var gmailService = new GmailService();
-            await gmailService.SendEmailAsync(client.Email, subject, body);
+            var clientGmailService = new ClientGmailService();
+            await clientGmailService.FormatLoyaltyLevelDowngradeEmail(client, loyaltyLevel, lostRewards);
 
             Console.WriteLine("Loyalty level downgrade email successfully sent.");
         }
