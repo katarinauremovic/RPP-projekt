2ac33b7212341e0857e4fc8aad0b9a8f2ad1444d
nlastro22 <nlastro22@student.foi.hr>
2025-01-31 20:39:47 +0100

    Kreirano korisničko sučelje za prijavu korisničkim imenom i lozinkom #19

---
+++ Software/GlamOfficeSoftware/PresentationLayer/Windows/LoginWithCredentials.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginWithCredentials.xaml
+    /// </summary>
+    public partial class LoginWithCredentials : Window
+    {
+        public LoginWithCredentials()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
