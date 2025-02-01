e59720c74d7210c34b2de122361820d40e21d64d
Anamarija Dominiković <adominiko22@student.foi.hr>
2025-02-01 23:27:26 +0100

    Implementiran dizajn SideBar-a za uređivanje podataka o zaposlenicima. #18

---
+++ Software/GlamOfficeSoftware/PresentationLayer/UserControls/ucEditEmployeeDetails.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ucEditEmployeeDetails.xaml
+    /// </summary>
+    public partial class ucEditEmployeeDetails : UserControl
+    {
+        public ucEditEmployeeDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCloseSidebar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
