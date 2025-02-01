dc761de2468fb51e3db04baef637e10e8585df76
nlastro22 <nlastro22@student.foi.hr>
2025-02-01 14:12:40 +0100

    Kreiran EmployeeDTO i EmployeeRepository s atributima i metodama #19

---
+++ Software/GlamOfficeSoftware/DataAccessLayer/Repositories/EmployeeRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Interfaces;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Data.Entity;
+namespace DataAccessLayer.Repositories
+{
+    public class EmployeeRepository : Repository<Employee>
+    {
+        public async Task<Employee> GetEmployeeByUsernameAndPasswordAsync(string username, string password)
+        {
+            var employee = await items.FirstOrDefaultAsync(e => e.Username == username);
+            if (employee == null)
+            {
+                return null;
+            }
+
+            string hashedPassword = HashPassword(employee.Salt, password);
+            if (hashedPassword == employee.Password)
+            {
+                return employee;
+            }
+            return null;
+        }
+
+        private string HashPassword(string salt, string password)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(salt + password);
+                byte[] hashedBytes = sha512.ComputeHash(inputBytes);
+                return Convert.ToBase64String(hashedBytes); 
+            }
+        }
+
+    }
+}
---
+++ Software/GlamOfficeSoftware/EntityLayer/DTOs/EmployeeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.DTOs
+{
+    public class EmployeeDTO
+    {
+        public int idEmployee { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public int? Role_idRole { get; set; }
+    }
+}
