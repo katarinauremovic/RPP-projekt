ffff7820362ce4f92d60141d3a6fd1664ce02439
Anamarija Dominiković <adominiko22@student.foi.hr>
2025-02-01 16:22:42 +0100

    Implementiran dizajn SideBar-a za dodavanje novih zaposlenika. #18

---
+++ Software/GlamOfficeSoftware/PresentationLayer/UserControls/ucAddNewEmployee.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ucAddNewEmployee.xaml
+    /// </summary>
+    public partial class ucAddNewEmployee : UserControl
+    {
+        public ucAddNewEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCloseSidebar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddNewEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
