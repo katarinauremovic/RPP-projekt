f32fa5074a0666c28e2e2505726d2906d8dba324
Katarina Uremović <kuremovic22@student.foi.hr>
2025-02-01 12:26:02 +0100

    Napravljen početni prikaz svih tretmana. #17

---
+++ Software/GlamOfficeSoftware/DataAccessLayer/Repositories/TreatmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Interfaces;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class TreatmentRepository : Repository<Treatment>, ITreatmentRepository
+    {
+       
+        public async Task<string> GetTreatmentGroupNameByIdAsync(int treatmentId)
+        {
+            var result = await (from t in items
+                                join tg in context.TreatmentGroups
+                                on t.TreatmentGroup_idTreatmentGroup equals tg.idTreatmentGroup
+                                where t.idTreatment == treatmentId
+                                select tg.Name)
+                                .FirstOrDefaultAsync();
+
+            return result ?? "No Group"; 
+        }
+
+        public async Task<IEnumerable<Treatment>> GetTreatmentsByNamePattern(string namePattern)
+        {
+            return await items
+                .Where(t => t.Name.Contains(namePattern)) 
+                .ToListAsync();
+        }
+
+
+    }
+}
