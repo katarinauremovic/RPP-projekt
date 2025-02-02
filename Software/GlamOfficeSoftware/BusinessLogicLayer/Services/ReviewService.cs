983c87b1ddb543128309e0679c0c3be21176d4eb
Katarina Uremović <kuremovic22@student.foi.hr>
2025-02-02 16:05:03 +0100

    Dodan service za rad sa recenzijama. #23

---
+++ Software/GlamOfficeSoftware/BusinessLogicLayer/Services/ReviewService.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class ReviewService
+    {
+        private readonly ReviewRepository _reviewRepository;
+
+        public ReviewService()
+        {
+            _reviewRepository = new ReviewRepository();
+        }
+
+        public async Task<Dictionary<int, int>> GetReviewDistributionAsync()
+        {
+            return await _reviewRepository.GetReviewDistributionAsync();
+        }
+
+        public async Task<Dictionary<int, double>> GetAverageRatingByEmployeeAsync()
+        {
+            return await _reviewRepository.GetAverageRatingByEmployeeAsync();
+        }
+
+        public async Task<Dictionary<string, int>> GetReviewTrendsOverTimeAsync()
+        {
+            return await _reviewRepository.GetReviewTrendsOverTimeAsync();
+        }
+
+        public async Task<IEnumerable<Review>> GetReviewsByEmployeeIdAsync(int employeeId)
+        {
+            return await _reviewRepository.GetReviewsByEmployeeIdAsync(employeeId);
+        }
+    }
+}
